aad0252e8b49e83fe5abf954a5c808281cf7de28
Mike Steel <michael.steel@smartpension.co.uk>
2023-05-11 11:44:29 +0100

    delete get

--- src/controllers/root/getRoot.js
+++ src/controllers/root/getRoot.js
@@ -8,15 +8,7 @@ import { getRandomRestaurant } from '../../utils/food'
  * @param {import('express').Response} res 
  */
 const getRoot = (_req, res) => {
-    const restaurant = getRandomRestaurant()
-
-    axios.post(`https://hooks.slack.com/services/${process.env.WEBHOOK}`, {
-        text: `How about ${restaurant.name}? They serve ${restaurant.type} food and are only ${restaurant.distance} away. ${restaurant.links ? `<https://goo.gl/maps/${restaurant.links.google}|More info here>` : ''}`,
-        username: 'lunchbot',
-        icon_emoji: ":gravyboatboatjeff:"
-    })
-
-    res.status(200).send('I have send a recommendation to #pints-or-lunch')
+    res.status(200).send('good')
 }
 
 export default getRoot
